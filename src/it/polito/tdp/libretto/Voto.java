aa6d9ef01dbbf76204ba930f4ba28cd7c6ffc779
matti <matti@DESKTOP-F58T2AI>
2019-03-11 13:52:28 +0100

    aggiunto metodi e costruttore

--- src/it/polito/tdp/libretto/Voto.java
+++ src/it/polito/tdp/libretto/Voto.java
@@ -1,5 +1,40 @@
 package it.polito.tdp.libretto;
 
+import java.time.LocalDate;
+
 public class Voto {
+	private int votoOttenuto;
+	private String nomeCorso;
+	private LocalDate data; // java.util.date o java.util.calendar più BUG che bit
+	
+	
+	public Voto(int votoOttenuto, String nomeCorso, LocalDate data) {
+		super();
+		this.votoOttenuto = votoOttenuto;
+		this.nomeCorso = nomeCorso;
+		this.data = data;
+	}
+
+	
+	public int getVotoOttenuto() {
+		return votoOttenuto;
+	}
+	public void setVotoOttenuto(int votoOttenuto) {
+		this.votoOttenuto = votoOttenuto;
+	}
+	public String getNomeCorso() {
+		return nomeCorso;
+	}
+	public void setNomeCorso(String nomeCorso) {
+		this.nomeCorso = nomeCorso;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
 
 }
